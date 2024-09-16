f393b85a65dbd190906ce8bb1d2be14dd8201db8
santanu panda <santanup1212@gmail.com>
2024-09-16 06:25:09 +0530

    Add the custom exception.

---
+++ src/main/java/com/qa/apiautomation/exp/APIFWPOC.java
@@ -0,0 +1,13 @@
+package com.qa.apiautomation.exp;
+
+public class APIFWPOC extends RuntimeException{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1974447717304195846L;
+
+	public APIFWPOC(String message) {
+		super(message);
+	}
+}

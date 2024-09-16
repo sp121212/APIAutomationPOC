85c24c8050ddbae1105f25632cfbaef440499f64
santanu panda <santanup1212@gmail.com>
2024-09-16 08:01:04 +0530

    did a changes

--- src/main/java/test/CheckResult.java
+++ src/main/java/test/CheckResult.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class CheckResult {
 	public static void main(String[] args) {
 		
+		@SuppressWarnings("resource")
 		Scanner scanner=new Scanner(System.in);
 		
 		

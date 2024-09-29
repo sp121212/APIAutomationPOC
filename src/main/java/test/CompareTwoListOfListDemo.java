5272d33a5a276e51ccb2970ee504076a387f6920
santanu panda <santanup1212@gmail.com>
2024-09-29 15:13:48 +0530

    Formatting changes

--- src/main/java/test/CompareTwoListOfListDemo.java
+++ src/main/java/test/CompareTwoListOfListDemo.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-//
+
 public class CompareTwoListOfListDemo {
-//
-	//
+	
 	public static void main(String[] args) {
 
 		List<List<Integer>> act = new ArrayList<List<Integer>>();
